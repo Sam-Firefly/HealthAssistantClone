c3bb5e31de97a5047a6f626ffb05cb93cd86eefa
Sam-Firefly <lzc897357343@gmail.com>
2024-10-28 11:12:25 +0800

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('.send-button').addEventListener('click', () => {
-    const input = document.querySelector('input[type="text"]');
-    if (input.value) {
-        alert(`You entered: ${input.value}`);
-        input.value = '';
-    }
-});
